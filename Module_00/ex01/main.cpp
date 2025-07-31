2a7ee8c163a6f8435d8d821588ae337b07201d8d
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 12:01:10 -0300

    style(main): optimize includes and variable declarations

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -6,31 +6,30 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 07:57:19 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/07/31 08:39:40 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/07/31 11:26:17 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include "PhoneBook.hpp"
 
 std::string getInput(const std::string &prompt)
 {
-	std::string input;
-	bool valid = false;
+	bool	valid;
 
+	std::string input;
+	valid = false;
 	while (!valid)
 	{
 		std::cout << prompt;
 		std::getline(std::cin, input);
-
 		if (std::cin.eof())
 		{
 			std::cout << "\nExiting program due to EOF." << std::endl;
 			exit(0);
 		}
-
 		if (!input.empty())
 		{
 			valid = true;
@@ -40,38 +39,33 @@ std::string getInput(const std::string &prompt)
 			std::cout << "Field cannot be empty. Please try again." << std::endl;
 		}
 	}
-	return input;
+	return (input);
 }
 
-int main()
+int	main(void)
 {
-	PhoneBook phoneBook;
-	std::string command;
+	PhoneBook	phoneBook;
+	Contact		newContact;
 
+	std::string command;
 	std::cout << "Welcome to the PhoneBook program!" << std::endl;
 	std::cout << "Available commands: ADD, SEARCH, EXIT" << std::endl;
-
 	while (true)
 	{
 		std::cout << "\nEnter a command: ";
 		std::getline(std::cin, command);
-
 		if (std::cin.eof())
 		{
 			std::cout << "\nExiting program due to EOF." << std::endl;
-			break;
+			break ;
 		}
-
 		if (command == "ADD")
 		{
-			Contact newContact;
-
 			newContact.setFirstName(getInput("Enter first name: "));
 			newContact.setLastName(getInput("Enter last name: "));
 			newContact.setNickname(getInput("Enter nickname: "));
 			newContact.setPhoneNumber(getInput("Enter phone number: "));
 			newContact.setDarkestSecret(getInput("Enter darkest secret: "));
-
 			if (newContact.isComplete())
 			{
 				phoneBook.addContact(newContact);
@@ -85,12 +79,11 @@ int main()
 		else if (command == "EXIT")
 		{
 			std::cout << "Exiting the PhoneBook program. Goodbye!" << std::endl;
-			break;
+			break ;
 		}
 		else
 		{
 		}
 	}
-
-	return 0;
+	return (0);
 }
