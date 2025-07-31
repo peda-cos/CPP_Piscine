d137a6daa4511badcaafcb3c2a7f78f0accb0e29
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 15:05:27 -0300

    feat: implement zombieHorde array allocation function

---
+++ Module_01/ex01/zombieHorde.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 11:30:17 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/07/31 11:37:58 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie	*zombieHorde(int N, std::string name)
+{
+	Zombie	*horde;
+
+	if (N <= 0)
+		return (NULL);
+	horde = new Zombie[N];
+	for (int i = 0; i < N; i++)
+	{
+		horde[i].setName(name);
+	}
+	return (horde);
+}
