43aedfba1ac0802d2e8842e1dd9c67339c36fe54
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 16:10:13 -0300

    feat: add Harl class header with complaint system interface

---
+++ Module_01/ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 15:59:29 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/07/31 16:07:52 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
+
+class Harl
+{
+  private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+
+  public:
+	Harl(void);
+	~Harl(void);
+	void complain(std::string level);
+};
+
+#endif
