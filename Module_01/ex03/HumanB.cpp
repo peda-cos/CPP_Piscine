ecb39b92d0beb04f5f700eaa4d8e55dea28cb535
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 15:33:42 -0300

    feat: implement HumanB class with optional weapon pointer

---
+++ Module_01/ex03/HumanB.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 15:15:46 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/07/31 15:29:07 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL)
+{
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	_weapon = &weapon;
+}
+
+void HumanB::attack() const
+{
+	if (_weapon)
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	else
+		std::cout << _name << " has no weapon to attack with" << std::endl;
+}
