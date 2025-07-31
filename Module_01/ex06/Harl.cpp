6c5bad1181bf812cc25ae5255e355c87911978b4
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 16:24:53 -0300

    feat: implement Harl logging class with level filtering

---
+++ Module_01/ex06/Harl.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 16:11:17 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/07/31 16:20:48 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::debug(void)
+{
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon..." << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money..." << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free..." << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*functions[4])(void) = {&Harl::debug, &Harl::info,
+		&Harl::warning, &Harl::error};
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*functions[i])();
+			return ;
+		}
+	}
+}
+
+void Harl::filter(std::string level)
+{
+	int	levelIndex;
+
+	levelIndex = 4;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			levelIndex = i;
+			break ;
+		}
+	}
+	switch (levelIndex)
+	{
+	case 0:
+		this->debug();
+	case 1:
+		this->info();
+	case 2:
+		this->warning();
+	case 3:
+		this->error();
+		break ;
+	default:
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
