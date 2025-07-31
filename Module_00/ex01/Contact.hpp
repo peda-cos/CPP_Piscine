faaa1d5c600291c7f7661f56a46d1a794fdb6787
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 12:01:10 -0300

    style(contact): apply 42 header formatting standards

--- Module_00/ex01/Contact.hpp
+++ Module_00/ex01/Contact.hpp
@@ -6,25 +6,25 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 08:01:33 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/07/31 08:18:50 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/07/31 11:25:56 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
-#define CONTACT_HPP
+# define CONTACT_HPP
 
-#include <string>
+# include <string>
 
 class Contact
 {
-private:
+  private:
 	std::string firstName;
 	std::string lastName;
 	std::string nickname;
 	std::string phoneNumber;
 	std::string darkestSecret;
 
-public:
+  public:
 	Contact();
 
 	std::string getFirstName() const;
