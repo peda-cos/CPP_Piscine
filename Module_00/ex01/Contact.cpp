c5f8211eaa6b1616170c4f0dc4769d22edbe3791
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-07-31 08:19:17 -0300

    feat: implement Contact class with data encapsulation

---
+++ Module_00/ex01/Contact.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 08:01:30 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/07/31 08:18:48 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+Contact::Contact() {}
+
+std::string Contact::getFirstName() const
+{
+	return firstName;
+}
+
+std::string Contact::getLastName() const
+{
+	return lastName;
+}
+
+std::string Contact::getNickname() const
+{
+	return nickname;
+}
+
+std::string Contact::getPhoneNumber() const
+{
+	return phoneNumber;
+}
+
+std::string Contact::getDarkestSecret() const
+{
+	return darkestSecret;
+}
+
+void Contact::setFirstName(const std::string &firstName)
+{
+	this->firstName = firstName;
+}
+
+void Contact::setLastName(const std::string &lastName)
+{
+	this->lastName = lastName;
+}
+
+void Contact::setNickname(const std::string &nickname)
+{
+	this->nickname = nickname;
+}
+
+void Contact::setPhoneNumber(const std::string &phoneNumber)
+{
+	this->phoneNumber = phoneNumber;
+}
+
+void Contact::setDarkestSecret(const std::string &darkestSecret)
+{
+	this->darkestSecret = darkestSecret;
+}
+
+bool Contact::isComplete() const
+{
+	return !firstName.empty() && !lastName.empty() && !nickname.empty() &&
+		   !phoneNumber.empty() && !darkestSecret.empty();
+}
