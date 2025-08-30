e3882525406abd110deb79851efd4be535e14b65
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 09:59:08 -0300

    refactor(ex00): remove debug output from Fixed class methods

--- Module_02/ex00/Fixed.cpp
+++ Module_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 09:42:30 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/01 09:52:29 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/30 09:55:05 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 
 Fixed::Fixed() : _fixedPointValue(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &other)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 	{
 		this->_fixedPointValue = other.getRawBits();
@@ -35,12 +32,10 @@ Fixed &Fixed::operator=(const Fixed &other)
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_fixedPointValue);
 }
 
