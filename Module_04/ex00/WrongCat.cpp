3545c23bdc236de8aba2ef36a63972d067b524ce
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 10:36:35 -0300

    feat: implement WrongCat with shadowed makeSound method

---
+++ Module_04/ex00/WrongCat.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 21:51:54 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/29 21:51:57 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() {
+    this->type = "WrongCat";
+    std::cout << "[WrongCat] Default constructor" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other) {
+    std::cout << "[WrongCat] Copy constructor" << std::endl;
+    *this = other;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other) {
+    std::cout << "[WrongCat] Copy assignment" << std::endl;
+    if (this != &other)
+        this->type = other.type;
+    return *this;
+}
+
+WrongCat::~WrongCat() {
+    std::cout << "[WrongCat] Destructor" << std::endl;
+}
+
+void WrongCat::makeSound() const {
+    std::cout << "(wrong cat attempt at meow)" << std::endl;
+}
