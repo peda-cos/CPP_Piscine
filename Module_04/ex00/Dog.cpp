cc42587b609e20921ebaa0e9f34a551e59e5cf35
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 10:36:35 -0300

    feat: implement Dog makeSound with woof behavior

---
+++ Module_04/ex00/Dog.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 21:50:03 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/30 10:34:09 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog() {
+    this->type = "Dog";
+    std::cout << "[Dog] Default constructor" << std::endl;
+}
+
+Dog::Dog(const Dog& other) : Animal(other) {
+    std::cout << "[Dog] Copy constructor" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& other) {
+    std::cout << "[Dog] Copy assignment" << std::endl;
+    if (this != &other) {
+        this->type = other.type;
+    }
+    return *this;
+}
+
+Dog::~Dog() {
+    std::cout << "[Dog] Destructor" << std::endl;
+}
+
+void Dog::makeSound() const {
+    std::cout << "Woof!" << std::endl;
+}
