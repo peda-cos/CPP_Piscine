430ab158c7ba3ce3c199d20b3f5d00da0bdec9f2
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 10:24:35 -0300

    feat: add ScavTrap header with virtual inheritance

---
+++ Module_03/ex03/ScavTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 20:11:27 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/30 10:20:39 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public virtual ClapTrap
+{
+  public:
+	ScavTrap();
+	ScavTrap(const std::string &name);
+	ScavTrap(const ScavTrap &other);
+	ScavTrap &operator=(const ScavTrap &other);
+	virtual ~ScavTrap();
+
+	virtual void attack(const std::string &target);
+	void guardGate();
+};
+
+#endif
