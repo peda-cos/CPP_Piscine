f0c69e025e82c77fe923a88ba7f213baf9a258f9
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 10:24:35 -0300

    feat: add FragTrap header with virtual inheritance

---
+++ Module_03/ex03/FragTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 20:11:04 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/30 10:20:22 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class FragTrap : public virtual ClapTrap
+{
+  public:
+	FragTrap();
+	FragTrap(const std::string &name);
+	FragTrap(const FragTrap &other);
+	FragTrap &operator=(const FragTrap &other);
+	virtual ~FragTrap();
+
+	void highFivesGuys();
+};
+
+#endif
