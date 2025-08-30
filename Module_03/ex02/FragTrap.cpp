5cff4a72104c93807b4adae23da68124d8e5b651
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 10:19:27 -0300

    feat: implement FragTrap derived class with high five ability

---
+++ Module_03/ex02/FragTrap.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 20:10:58 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/30 10:18:05 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap("Unnamed")
+{
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+	std::cout << "FragTrap " << _name << " constructed!" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string &name) : ClapTrap(name)
+{
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+	std::cout << "FragTrap " << _name << " constructed!" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &other) : ClapTrap(other)
+{
+	std::cout << "FragTrap " << _name << " constructed!" << std::endl;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &other)
+{
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+	}
+	return (*this);
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "FragTrap " << _name << " destroyed!" << std::endl;
+}
+
+void FragTrap::highFivesGuys()
+{
+	std::cout << "FragTrap " << _name << " requests positive high fives!" << std::endl;
+}
