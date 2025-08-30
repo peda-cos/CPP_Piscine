4846ee65047a2603cfde617ee72e0989f8206ea3
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-30 10:24:35 -0300

    feat: implement ScavTrap with gate keeper functionality

---
+++ Module_03/ex03/ScavTrap.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 20:06:38 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/30 10:20:30 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap("Unnamed")
+{
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap " << _name << " constructed!" << std::endl;
+}
+
+ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name)
+{
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap " << _name << " constructed!" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
+{
+	std::cout << "ScavTrap " << _name << " constructed!" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other)
+{
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+	}
+	return (*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << _name << " destroyed!" << std::endl;
+}
+
+void ScavTrap::attack(const std::string &target)
+{
+	if (_hitPoints == 0 || _energyPoints == 0)
+		return;
+	_energyPoints--;
+	std::cout << "ScavTrap " << _name << " attacks " << target << ", causing "
+			  << _attackDamage << " points of damage!" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << _name << " is now in Gate keeper mode!" << std::endl;
+}
