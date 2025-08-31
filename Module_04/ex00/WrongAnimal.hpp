fe08f0ed7872dea0e89411fcb126346437f48bd6
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat WrongAnimal header to 42 standard

--- Module_04/ex00/WrongAnimal.hpp
+++ Module_04/ex00/WrongAnimal.hpp
@@ -6,27 +6,29 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:50:55 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 15:08:42 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:26:38 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WRONGANIMAL_HPP
-#define WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
 
-#include <iostream>
-#include <string>
+# include <iostream>
+# include <string>
 
-class WrongAnimal {
-protected:
-    std::string type;
-public:
-    WrongAnimal();
-    WrongAnimal(const WrongAnimal& other);
-    WrongAnimal& operator=(const WrongAnimal& other);
-    ~WrongAnimal();
+class WrongAnimal
+{
+  protected:
+	std::string type;
 
-    void        makeSound() const;
-    std::string getType() const;
+  public:
+	WrongAnimal();
+	WrongAnimal(const WrongAnimal &other);
+	WrongAnimal &operator=(const WrongAnimal &other);
+	~WrongAnimal();
+
+	void makeSound() const;
+	std::string getType() const;
 };
 
 #endif
