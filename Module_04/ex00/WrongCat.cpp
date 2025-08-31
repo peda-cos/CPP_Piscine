21de2d60a8e9e95bd2ef7256c9234cf74cbc6eae
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat WrongCat class to 42 standard

--- Module_04/ex00/WrongCat.cpp
+++ Module_04/ex00/WrongCat.cpp
@@ -6,32 +6,37 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:51:54 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 15:09:46 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:27:39 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongCat.hpp"
 
-WrongCat::WrongCat() {
-    this->type = "WrongCat";
-    std::cout << "[WrongCat] Default constructor" << std::endl;
+WrongCat::WrongCat()
+{
+	this->type = "WrongCat";
+	std::cout << "[WrongCat] Default constructor" << std::endl;
 }
 
-WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other) {
-    std::cout << "[WrongCat] Copy constructor" << std::endl;
+WrongCat::WrongCat(const WrongCat &other) : WrongAnimal(other)
+{
+	std::cout << "[WrongCat] Copy constructor" << std::endl;
 }
 
-WrongCat& WrongCat::operator=(const WrongCat& other) {
-    std::cout << "[WrongCat] Copy assignment" << std::endl;
-    if (this != &other)
-        this->type = other.type;
-    return *this;
+WrongCat &WrongCat::operator=(const WrongCat &other)
+{
+	std::cout << "[WrongCat] Copy assignment" << std::endl;
+	if (this != &other)
+		this->type = other.type;
+	return (*this);
 }
 
-WrongCat::~WrongCat() {
-    std::cout << "[WrongCat] Destructor" << std::endl;
+WrongCat::~WrongCat()
+{
+	std::cout << "[WrongCat] Destructor" << std::endl;
 }
 
-void WrongCat::makeSound() const {
-    std::cout << "(wrong cat attempt at meow)" << std::endl;
+void WrongCat::makeSound() const
+{
+	std::cout << "(wrong cat attempt at meow)" << std::endl;
 }
