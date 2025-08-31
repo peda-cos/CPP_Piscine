d45729666b9c73cb8fb5da648d3d9e6c032b68d4
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat Dog header to 42 coding standard

--- Module_04/ex00/Dog.hpp
+++ Module_04/ex00/Dog.hpp
@@ -6,23 +6,24 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:49:06 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 10:28:23 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:26:34 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DOG_HPP
-#define DOG_HPP
+# define DOG_HPP
 
-#include "Animal.hpp"
+# include "Animal.hpp"
 
-class Dog : public Animal {
-public:
-    Dog();
-    Dog(const Dog& other);
-    Dog& operator=(const Dog& other);
-    virtual ~Dog();
+class Dog : public Animal
+{
+  public:
+	Dog();
+	Dog(const Dog &other);
+	Dog &operator=(const Dog &other);
+	virtual ~Dog();
 
-    virtual void makeSound() const;
+	virtual void makeSound() const;
 };
 
 #endif
