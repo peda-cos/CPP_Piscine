e3b8901368da8395738da0e88ac4786f23a2c2cf
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat Cat class to 42 coding standard

--- Module_04/ex00/Cat.cpp
+++ Module_04/ex00/Cat.cpp
@@ -6,33 +6,39 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:50:19 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 10:34:09 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:27:01 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
-Cat::Cat() {
-    this->type = "Cat";
-    std::cout << "[Cat] Default constructor" << std::endl;
+Cat::Cat()
+{
+	this->type = "Cat";
+	std::cout << "[Cat] Default constructor" << std::endl;
 }
 
-Cat::Cat(const Cat& other) : Animal(other) {
-    std::cout << "[Cat] Copy constructor" << std::endl;
+Cat::Cat(const Cat &other) : Animal(other)
+{
+	std::cout << "[Cat] Copy constructor" << std::endl;
 }
 
-Cat& Cat::operator=(const Cat& other) {
-    std::cout << "[Cat] Copy assignment" << std::endl;
-    if (this != &other) {
-        this->type = other.type;
-    }
-    return *this;
+Cat &Cat::operator=(const Cat &other)
+{
+	std::cout << "[Cat] Copy assignment" << std::endl;
+	if (this != &other)
+	{
+		this->type = other.type;
+	}
+	return (*this);
 }
 
-Cat::~Cat() {
-    std::cout << "[Cat] Destructor" << std::endl;
+Cat::~Cat()
+{
+	std::cout << "[Cat] Destructor" << std::endl;
 }
 
-void Cat::makeSound() const {
-    std::cout << "Meow!" << std::endl;
+void Cat::makeSound() const
+{
+	std::cout << "Meow!" << std::endl;
 }
