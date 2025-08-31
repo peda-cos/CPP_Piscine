0af25c18a45f5c80fdb618ad491834d796554084
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat Dog class to 42 coding standard

--- Module_04/ex00/Dog.cpp
+++ Module_04/ex00/Dog.cpp
@@ -6,33 +6,39 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:50:03 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 10:34:09 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:27:06 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
-Dog::Dog() {
-    this->type = "Dog";
-    std::cout << "[Dog] Default constructor" << std::endl;
+Dog::Dog()
+{
+	this->type = "Dog";
+	std::cout << "[Dog] Default constructor" << std::endl;
 }
 
-Dog::Dog(const Dog& other) : Animal(other) {
-    std::cout << "[Dog] Copy constructor" << std::endl;
+Dog::Dog(const Dog &other) : Animal(other)
+{
+	std::cout << "[Dog] Copy constructor" << std::endl;
 }
 
-Dog& Dog::operator=(const Dog& other) {
-    std::cout << "[Dog] Copy assignment" << std::endl;
-    if (this != &other) {
-        this->type = other.type;
-    }
-    return *this;
+Dog &Dog::operator=(const Dog &other)
+{
+	std::cout << "[Dog] Copy assignment" << std::endl;
+	if (this != &other)
+	{
+		this->type = other.type;
+	}
+	return (*this);
 }
 
-Dog::~Dog() {
-    std::cout << "[Dog] Destructor" << std::endl;
+Dog::~Dog()
+{
+	std::cout << "[Dog] Destructor" << std::endl;
 }
 
-void Dog::makeSound() const {
-    std::cout << "Woof!" << std::endl;
+void Dog::makeSound() const
+{
+	std::cout << "Woof!" << std::endl;
 }
