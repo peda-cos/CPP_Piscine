b3b3e53ebed82b6424268154f466bf42136fb1bf
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat WrongCat header to 42 standard

--- Module_04/ex00/WrongCat.hpp
+++ Module_04/ex00/WrongCat.hpp
@@ -6,23 +6,24 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:51:17 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 15:08:45 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:26:40 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WRONGCAT_HPP
-#define WRONGCAT_HPP
+# define WRONGCAT_HPP
 
-#include "WrongAnimal.hpp"
+# include "WrongAnimal.hpp"
 
-class WrongCat : public WrongAnimal {
-public:
-    WrongCat();
-    WrongCat(const WrongCat& other);
-    WrongCat& operator=(const WrongCat& other);
-    ~WrongCat();
+class WrongCat : public WrongAnimal
+{
+  public:
+	WrongCat();
+	WrongCat(const WrongCat &other);
+	WrongCat &operator=(const WrongCat &other);
+	~WrongCat();
 
-    void makeSound() const;
+	void makeSound() const;
 };
 
 #endif
