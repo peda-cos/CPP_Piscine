ac88d4786b8536f7aa83ba66d66d446fd014299a
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-31 12:30:23 -0300

    style: reformat Cat header to 42 coding standard

--- Module_04/ex00/Cat.hpp
+++ Module_04/ex00/Cat.hpp
@@ -6,23 +6,24 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:48:55 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/30 10:28:13 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/31 12:26:31 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAT_HPP
-#define CAT_HPP
+# define CAT_HPP
 
-#include "Animal.hpp"
+# include "Animal.hpp"
 
-class Cat : public Animal {
-public:
-    Cat();
-    Cat(const Cat& other);
-    Cat& operator=(const Cat& other);
-    virtual ~Cat();
+class Cat : public Animal
+{
+  public:
+	Cat();
+	Cat(const Cat &other);
+	Cat &operator=(const Cat &other);
+	virtual ~Cat();
 
-    virtual void makeSound() const;
+	virtual void makeSound() const;
 };
 
 #endif
