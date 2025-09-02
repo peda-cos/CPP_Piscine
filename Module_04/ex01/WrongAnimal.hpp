9abb246fa74df387fd0b40f692bd5ea4bd9ec0d8
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-09-02 03:26:55 -0300

    feat(WrongAnimal): add base class without virtual methods

---
+++ Module_04/ex01/WrongAnimal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 21:50:55 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/31 12:26:38 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
+
+# include <iostream>
+# include <string>
+
+class WrongAnimal
+{
+  protected:
+	std::string type;
+
+  public:
+	WrongAnimal();
+	WrongAnimal(const WrongAnimal &other);
+	WrongAnimal &operator=(const WrongAnimal &other);
+	~WrongAnimal();
+
+	void makeSound() const;
+	std::string getType() const;
+};
+
+#endif
