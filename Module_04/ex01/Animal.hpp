ff609c6870f3359521336ce4b4bf9b42ab83df8a
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-09-02 03:26:54 -0300

    refactor: declare makeSound as pure virtual in Animal

---
+++ Module_04/ex01/Animal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 21:47:07 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/31 12:26:27 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+# include <string>
+
+class Animal
+{
+  protected:
+	std::string type;
+
+  public:
+	Animal();
+	Animal(const Animal &other);
+	Animal &operator=(const Animal &other);
+	virtual ~Animal();
+
+	virtual void makeSound() const = 0;
+	std::string getType() const;
+};
+
+#endif
