ffd37ebd4165465031976af82c7c1b8ce4d357fd
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-09-10 20:28:39 -0300

    refactor: restructure main with proper instantiation tests

--- Module_04/ex00/main.cpp
+++ Module_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 21:52:30 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/31 12:28:43 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/09/10 20:26:37 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,65 +16,54 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
-static void	separator(const std::string &title)
+static void separator(const std::string &title)
 {
-	std::cout << "\n==== " << title << " ====\n";
+	std::cout << "\n==== " << title << " ====" << std::endl;
 }
 
-int	main(void)
+int main()
 {
-		const Animal *j = new Dog();
-		const Animal *i = new Cat();
-		delete j;
-		delete i;
-		const size_t N = 4;
-		const Animal *zoo[N];
-			delete zoo[idx];
-		Dog a;
-		Dog c;
-		Cat x;
-		Cat z;
-		const WrongAnimal *w = new WrongCat();
-		delete w;
-		WrongCat wc;
-
 	separator("Subject Basic Test");
-	{
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound();
-		j->makeSound();
-	}
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+
 	separator("Polymorphic Array Test");
+	const size_t N = 4;
+	const Animal* zoo[N];
+	for (size_t idx = 0; idx < N; ++idx)
 	{
-		for (size_t idx = 0; idx < N; ++idx)
-		{
-			if (idx % 2 == 0)
-				zoo[idx] = new Dog();
-			else
-				zoo[idx] = new Cat();
-		}
-		for (size_t idx = 0; idx < N; ++idx)
-		{
-			std::cout << idx << ": " << zoo[idx]->getType() << " -> ";
-			zoo[idx]->makeSound();
-		}
-		for (size_t idx = 0; idx < N; ++idx)
+		if (idx % 2 == 0)
+			zoo[idx] = new Dog();
+		else
+			zoo[idx] = new Cat();
 	}
-	separator("Copy Semantics Test");
+	for (size_t idx = 0; idx < N; ++idx)
 	{
-		Dog b(a);
-		c = b;
-		std::cout << "Types: a=" << a.getType() << ", b=" << b.getType() << ", c=" << c.getType() << std::endl;
-		Cat y(x);
-		z = y;
-		std::cout << "Types: x=" << x.getType() << ", y=" << y.getType() << ", z=" << z.getType() << std::endl;
+		std::cout << idx << ": " << zoo[idx]->getType() << " -> ";
+		zoo[idx]->makeSound();
 	}
+	for (size_t idx = 0; idx < N; ++idx)
+		delete zoo[idx];
+
 	separator("WrongAnimal Polymorphism Failure Demo");
-	{
-		w->makeSound();
-		wc.makeSound();
-	}
+	const WrongAnimal* w = new WrongCat();
+	const WrongCat wc;
+	std::cout << w->getType() << " " << std::endl;
+	w->makeSound();
+	wc.makeSound();
+	delete w;
+
 	separator("Done");
 	return (0);
 }
